bf7c7d7a6a2bf93eb67145d37c6d622d43f04e04
Vencent6666 <88521740+Vencent6666@users.noreply.github.com>
2022-05-23 22:47:02 +0800

    Add files via upload

---
+++ BLL/Hough.cs
@@ -0,0 +1,26 @@
+﻿using DotSpatial.Topology;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SGEDSystem
+{
+    class Hough
+    {
+        public double a { get; set; }
+        public double b { get; set; }
+        public double r { get; set; }
+        public List<Coordinate> p;
+        public int count { get; set; }
+        public Hough(double x0, double y0, double Radius, List<Coordinate> Points)   //构造函数
+        {
+            a = x0;
+            b = y0;
+            r = Radius;
+            p = Points;
+            count = 1;
+        }
+    }
+}
